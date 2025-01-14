0dd28d57f23fba53d44b4a68b7b96f0056d123b5
Sugarcanejuice <2026Dong.Leon@fcboe.org>
2025-01-14 13:40:45 -0500

    Update pht_sensor.py

--- pht_sensor.py
+++ pht_sensor.py
@@ -29,7 +29,6 @@ display_bus = FourWire(spi, command=tft_dc, chip_select=tft_cs)
 display = ST7789(
     display_bus, rotation=270, width=240, height=135, rowstart=40, colstart=53
 )
-
 splash = displayio.Group()
 display.root_group = splash
 
@@ -39,29 +38,6 @@ color_palette[0] = BACKGROUND_COLOR
 
 bg_sprite = displayio.TileGrid(color_bitmap, pixel_shader=color_palette, x=0, y=0)
 splash.append(bg_sprite)
-
-# Draw a smaller inner rectangle
-inner_bitmap = displayio.Bitmap(
-    display.width - BORDER * 2, display.height - BORDER * 2, 1
-)
-inner_palette = displayio.Palette(1)
-inner_palette[0] = FOREGROUND_COLOR
-inner_sprite = displayio.TileGrid(
-    inner_bitmap, pixel_shader=inner_palette, x=BORDER, y=BORDER
-)
-splash.append(inner_sprite)
-
-# Draw a label
-text = "Hello World!"
-text_area = label.Label(terminalio.FONT, text=text, color=TEXT_COLOR)
-text_width = text_area.bounding_box[2] * FONTSCALE
-text_group = displayio.Group(
-    scale=FONTSCALE,
-    x=display.width // 2 - text_width // 2,
-    y=display.height // 2,
-)
-text_group.append(text_area)
-splash.append(text_group)
 
 main_group = displayio.Group()
 display.root_group = main_group
